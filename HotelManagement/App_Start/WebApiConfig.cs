982481a37f42cc3e4d4e758b2dc854b0a845cf8c
hieulatrung2911 <hieu.latrung2911@gmail.com>
2022-08-19 15:28:22 +0700

    19.8.2022

---
+++ HotelManagement/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace HotelManagement
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+
+            // configure json formatter
+            JsonMediaTypeFormatter jsonFormatter = config.Formatters.JsonFormatter;
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ HotelManagement/Controllers/HotelController.cs
@@ -0,0 +1,54 @@
+﻿using HotelManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+
+namespace HotelManagement.Controllers
+{
+    public class HotelController : ApiController
+    {
+        public IHttpActionResult GetAllHotels()
+        {
+            IList<Hotel> hotels = null;
+            
+            using(var ctx = new HotelEntities())
+            {
+                hotels = ctx.Hotels.Include(s => s.Address)
+                                  .Include(s => s.Employees)
+                                  .Include(s => s.HotelChainHasHotels)
+                                  .Include(s => s.Rooms).AsNoTracking().Select(s => s).ToList();
+            }
+
+            if(hotels.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(hotels);
+        }
+
+        public IHttpActionResult GetHotelByID(int id)
+        {
+            Hotel hotel = new Hotel(){HotelID = 1, HotelName = "Royal"};
+
+            //using (var ctx = new HotelEntities())
+            //{
+            //    hotel = ctx.Hotels.Include("Address")
+            //                      .Include("Employees")
+            //                      .Include("HotelChainHasHotels")
+            //                      .Include("Rooms").AsNoTracking().FirstOrDefault(s => s.HotelID == id);
+            //}
+
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(hotel);
+        }
+    }
+}
